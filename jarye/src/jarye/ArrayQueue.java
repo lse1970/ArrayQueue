f33751049f48d0123a3c7a00dab7272a466a560e
lse1970 <dltmddms1970@naver.com>
2018-04-18 16:23:43 +0900

    Change-Id: I0000000000000000000000000000000000000000 queue
    Change-Id: I6ff3dca1b15d707373fb2015877694c17fdec9e3

---
+++ jarye/src/jarye/ArrayQueue.java
@@ -0,0 +1,23 @@
+package jarye;
+
+public class ArrayQueue {
+	static char queue[]  =new char[5];
+	static int rear=-1, front=-1;
+	public static void main(String[] args) {
+		enQueue('A');
+		enQueue('B');
+		enQueue('C');
+		deQueue();
+		deQueue();
+		deQueue();
+
+	}
+	static void enQueue(char data) {
+		queue[++rear] = data;
+	}
+	static void deQueue()	{
+		System.out.println(queue[++front]);
+	}
+	
+
+}
